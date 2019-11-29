54d0f7e7a7dac0fb383d3ae31c628b90cc42da6e
Tung windowPC <tung9295@gmail.com>
2019-11-29 16:01:27 +0900

    fix bug

--- index.js
+++ index.js
@@ -2,7 +2,33 @@ var express = require('express');
 var app = express();
 var port = 5000;
 
+var users= [
+    {id: 1, name: 'tung'},
+    {id: 2, name: 'tan'},
+    {id: 3, name: 'trung'}
+];
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 app.get('/', function(req, res) {
-    res.send('HELLO');
+    res.render('index');
 });
-app.listen(port, () => console.log('Listening on port ' + port));
+
+app.get('/users', function(req, res) {
+    res.render('users/index', {
+        users: users      
+    })
+});
+
+app.get('/users/search', function(req, res) {
+    var q = req.query.q;
+    var matchedUsers = users.filter(function(user) {
+        return user.name.indexOf(q) >= 0
+    });
+    res.render('users/index', {
+        users: matchedUsers
+    })
+});
+
+app.listen(port, () => console.log('Listening on port ' + port + '...'));
